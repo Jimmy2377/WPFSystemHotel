63177ab77be664e010c6dddac8bc6f805ae534e8
jimmy2377 <jimmymiguel23@gmail.com>
2023-05-10 20:22:48 -0400

    actualizado

--- Hotel/Data_layer/ConnectionToMysql.cs
+++ Hotel/Data_layer/ConnectionToMysql.cs
@@ -11,11 +11,11 @@ namespace Hotel.Data_layer
 {
     public abstract class ConnectionToMysql
     {
-        private readonly string servidor = "bx03xbcc3anbn4qledgy-mysql.services.clever-cloud.com"; // Reemplazar con el nombre del servidor de la base de datos remota
-        private readonly string puerto = "3306"; // Reemplazar con el puerto utilizado para la conexión a la base de datos
-        private readonly string usuario = "u1mm5br4gmrdryoa"; // Reemplazar con el nombre de usuario utilizado para la conexión a la base de datos
-        private readonly string password = "D33bt4Jje6LVYbpTRzms"; // Reemplazar con la contraseña utilizada para la conexión a la base de datos
-        private readonly string nombreDB = "bx03xbcc3anbn4qledgy"; // Reemplazar con el nombre de la base de datos remota
+        private readonly string servidor = "bx03xbcc3anbn4qledgy-mysql.services.clever-cloud.com";
+        private readonly string puerto = "3306"; 
+        private readonly string usuario = "u1mm5br4gmrdryoa"; 
+        private readonly string password = "D33bt4Jje6LVYbpTRzms";
+        private readonly string nombreDB = "bx03xbcc3anbn4qledgy"; 
         private readonly string connectionString;
         public ConnectionToMysql()
         {
